f902dcfd2181d33299c347565e72e1015bc943c8
Markus Kuhn <gope153@gmail.com>
2017-06-14 09:03:10 +0200

    added soy and vegan

--- docs/api/api_project.js
+++ docs/api/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-06-07T05:45:23.845Z",
+    "time": "2017-06-14T05:54:49.522Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
